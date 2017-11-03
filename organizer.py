afa7ad2acccb63c5db92c28e39533d208730be35
haris <hhaarree@hotmail.com>
2017-11-03 14:16:30 +0100

    Cleaning up

--- organizer.py
+++ organizer.py
@@ -1,8 +1,14 @@
+"""
+A bare bones script that organizes all the .mp3 files in the current directory by
+creating Artist/Album/ dirs and subdirs. Finally moving the .mp3 files into the
+newly created dirs they belong in
+"""
 import os
 import shutil
 
 
 def get_songs():
+    """Return a list containing the filenames of all the .mp3 files in the directory"""
     files = os.listdir()
     songs = []
     for file in files:
@@ -12,7 +18,7 @@ def get_songs():
 
 
 def load_tags(song):
-    """Load an mp3 file and decode it with utf-8 or latin1 encoding. 
+    """Load an mp3 file and decode it with utf-8 or latin1 encoding.
     Return a substring containing the Title, Artist and Album tags.
     """
     tags = song.read(128)
@@ -21,8 +27,7 @@ def load_tags(song):
     # check if the file actually has any ID3v2 tags
     if tags.startswith("ID3"):
         return tags
-    else:
-        return False
+    return None
 
 
 def sanitize_tag(tag):
@@ -47,10 +52,12 @@ def extract_tags(tags):
 
 
 def rename_song(file, artist, title):
+    """Rename file based on the artist and title arguments"""
     os.rename(file, "{} - {}.{}".format(artist, title, "mp3"))
 
 
-def create_dirs(artist, album):
+def create_dir(artist, album):
+    """Construct a path from the artist and album args and create the  """
     songdir = os.path.join(artist, album)
     os.makedirs(songdir, exist_ok=True)
     return songdir
@@ -64,7 +71,7 @@ def main():
 
         with open(song, "rb") as s:
             tags = load_tags(s)
-            if tags == False:
+            if tags is None:
                 print("No tags found in {}".format(song))
                 continue
 
@@ -77,7 +84,7 @@ def main():
 
             rename_song(song, artist, title)
 
-            songdir = create_dirs(artist, album)
+            songdir = create_dir(artist, album)
 
             shutil.move(song, songdir)
 
